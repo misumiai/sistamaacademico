4fe0cd8d96f6fcf52e57cec4ac7f0e2c7540df37
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-14 21:23:07 -0300

    Delete aula.py directory

--- aula.py/exercicio.aula3.py
+++
@@ -1,4 +0,0 @@
-mensagem : str = ""
-print("Fale alguma coisa")
-mensagem = input("aqui: ") 
-print("nao acredito que vc disse", mensagem, "D:")
--- aula.py/exercicio.aula4.py
+++
@@ -1,6 +0,0 @@
-
-print("***Dados do usuario***")
-
-nome = str (input("eu sou o(a): "))
-cidade = str (input("moro em: ")) 
-idade = str (input("tenho: "))
