16bec28fab760be5a8504f385db7df0e9f41ce0e
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-28 15:04:40 -0300

    Delete aula.py/questao.4.py

--- aula.py/questao.4.py
+++
@@ -1,9 +0,0 @@
-nume1: int = 0
-total: int = 0
-
-while True:
-    nume1 = int(input("digite um numero: "))
-    if nume1 < 0: 
-        break
-    total = total + nume1
-print("a soma total foi: ", total)
