23602f57b5ccce80ec8ec8bd2cfcbe06526630b6
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-28 15:04:51 -0300

    Delete aula.py/questao.5.py

--- aula.py/questao.5.py
+++
@@ -1,8 +0,0 @@
-num1: int = int(input("informe o numero: "))
-contador: int = 0 
-
-while contador <= 10:
-    tabuada: int = num1 * contador
-    print(f"tabuada de {num1} X {contador} é {tabuada}")
-    contador = contador + 1
-
