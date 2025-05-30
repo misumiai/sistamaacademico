2fc605e30113152a4a36c5f7414ff60756dfabb8
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-30 20:12:35 -0300

    Update aula24.py

--- aula.py/aula24.py
+++ aula.py/aula24.py
@@ -1,1 +1,12 @@
+#Função é um bloco de codigo que pode ser chamado
 
+def somar():
+  num1: int = int(input("informe um numero: "))
+  num2: int = int(input("informe um numero: "))
+
+  total: int = num1 + num2
+  print("A soma total é: ", total)
+
+somar()
+print("vamos somar novamente")
+somar()
