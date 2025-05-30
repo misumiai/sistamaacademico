02eff7cdfbf747e189e59a66238bb1fd61f5061f
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-30 20:25:34 -0300

    Update aula24.py

--- aula.py/aula24.py
+++ aula.py/aula24.py
@@ -7,6 +7,15 @@ def somar():
   total: int = num1 + num2
   print("A soma total é: ", total)
 
-somar()
+def subtrair():
+  num1: int = int(input("informe um numero: "))
+  num2: int = int(input("informe um numero: "))
+
+  total: int = num1 - num2
+  print("A soma total é: ", total)
+
+somar() #invocando a função
 print("vamos somar novamente")
-somar()
+somar() #invocando função
+subtrair() #invocando função
+
