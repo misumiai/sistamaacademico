34daf1f656dc3b934f2ba44919d31bb7dcd9ba67
Misumiai <72038408+misumiai@users.noreply.github.com>
2025-05-31 09:20:40 -0300

    Atualizar o atividade4.py

--- aula.py/atividade4.py
+++ aula.py/atividade4.py
@@ -26,7 +26,32 @@ def circulo():
   total: int = pi * (raio ** 2)
   print("O total do circulo é: ", total)
 
-quadrado()
+def mostrar_menu():
+    print("\n--- MENU ---")
+    print("1. calcular quadrado")
+    print("2. calcular triângulo")
+    print("3. calcular retângulo")
+    print("4. calcular circulo")
+    print("5. Sair")
+
+while True:
+    mostrar_menu()
+    escolha = input("Escolha uma opção (1-6): ")
+
+    if escolha == "1":
+  quadrado()
+
+elif escolha == "2":
 triangulo()
+
+elif escolha == "3":
 retangulo()
+
+elif escolha == "4":
 circulo()
+
+elif escolha == "5":
+print("você saiu do programa!")
+
+else:
+print("opção inválida")
